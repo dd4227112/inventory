06171dca41e391db3e1c9955423e519aa8e1d47b
David Daniel <dd4227112@gmail.com>
2023-12-28 13:05:55 +0300

    add soft delete

--- database/migrations/2013_12_15_202228_create_units_table.php
+++ database/migrations/2013_12_15_202228_create_units_table.php
@@ -19,7 +19,6 @@ return new class extends Migration
             $table->string('description')->nullable();
             $table->timestamps();
             $table->uuid('uuid');
-            $table->timestamps();
             $table->softDeletes();
         });
     }
